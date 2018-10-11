977f14e5b187be526e3ce921a528efbc24ce3d06
Luc van Dort <luc.van.dort@rhdhv.com>
2018-10-11 22:09:00 +0200

    flake8 big cleanup

--- powerplots.py
+++ powerplots.py
@@ -1,16 +1,19 @@
-import sys, time
+import sys
+import time
 import numpy as np
-import matplotlib as mpl
 
 from PyQt5 import uic
 from PyQt5.QtCore import QThread, QTimer, pyqtSignal
-from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
 
 def trap_exc_during_debug(*args):
     # when app raises uncaught exception, print info
     print(args)
 
+
 sys.excepthook = trap_exc_during_debug
+
 
 class playbackThread(QThread):
 
@@ -26,6 +29,7 @@ class playbackThread(QThread):
         while(True):
             self.sig_step.emit()
             time.sleep(0.1)
+
 
 class PowerPlotApp(QMainWindow):
 
@@ -45,7 +49,8 @@ class PowerPlotApp(QMainWindow):
         self.instantaneous_phase_angle.valueChanged.connect(self.update_plots)
 
         self.playback_button.clicked.connect(self.start_playback)
-        self.playback_reset_button.clicked.connect(self.set_instantaneous_phase)
+        self.playback_reset_button.clicked.connect(
+            self.set_instantaneous_phase)
 
     def start_playback(self):
         self.playback_reset_button.setEnabled(False)
@@ -55,7 +60,8 @@ class PowerPlotApp(QMainWindow):
         self.playback_button.clicked.connect(self.stop_playback)
 
         self.playback_thread = playbackThread()
-        self.playback_thread.sig_step.connect(self.increment_instantaneous_phase)
+        self.playback_thread.sig_step.connect(
+            self.increment_instantaneous_phase)
         self.playback_thread.start()
         self.playback_button.clicked.connect(self.playback_thread.terminate)
 
@@ -67,59 +73,84 @@ class PowerPlotApp(QMainWindow):
         self.playback_button.clicked.connect(self.start_playback)
 
     def init_phasor_plot(self):
-        self.phasor_plot.canvas.axes.set_ylim([-2,2])
-        self.phasor_plot.canvas.axes.set_xlim([-2,2])
+        self.phasor_plot.canvas.axes.set_ylim([-2, 2])
+        self.phasor_plot.canvas.axes.set_xlim([-2, 2])
         self.phasor_plot.canvas.axes.grid(True)
         self.phasor_plot.canvas.axes.axis('equal')
-        
+
         self.phasor_plot.canvas.axes.axhline(color='black', zorder=1, lw=1)
         self.phasor_plot.canvas.axes.axvline(color='black', zorder=1, lw=1)
-        unity_circle = np.exp(-1j*np.arange(0,2*np.pi,np.pi/180))
-        self.phasor_plot.canvas.axes.plot(np.real(unity_circle), np.imag(unity_circle), '0.2', zorder=-1, lw=1)
+        unity_circle = np.exp(-1j*np.arange(0, 2*np.pi, np.pi/180))
+        self.phasor_plot.canvas.axes.plot(
+            np.real(unity_circle),
+            np.imag(unity_circle), '0.2', zorder=-1, lw=1)
 
         self.phasor_lines = {}
-        self.phasor_lines['U'], = self.phasor_plot.canvas.axes.plot([0,0],[0,0], 'b', zorder=10, lw=3)
-        self.phasor_lines['I'], = self.phasor_plot.canvas.axes.plot([0,0],[0,0], 'r', zorder=20, lw=3)
-        self.phasor_lines['S'], = self.phasor_plot.canvas.axes.plot([0,0],[0,0], 'c', zorder=30, lw=3)
+        self.phasor_lines['U'], = self.phasor_plot.canvas.axes.plot(
+            [0, 0], [0, 0], 'b', zorder=10, lw=3)
+        self.phasor_lines['I'], = self.phasor_plot.canvas.axes.plot(
+            [0, 0], [0, 0], 'r', zorder=20, lw=3)
+        self.phasor_lines['S'], = self.phasor_plot.canvas.axes.plot(
+            [0, 0], [0, 0], 'c', zorder=30, lw=3)
 
         self.phasor_circles = {}
-        self.phasor_circles['U'], = self.phasor_plot.canvas.axes.plot(np.zeros(360), np.zeros(360), 'b', zorder=2, lw=1, ls='--')
-        self.phasor_circles['I'], = self.phasor_plot.canvas.axes.plot(np.zeros(360), np.zeros(360), 'r', zorder=3, lw=1, ls='--')
-        self.phasor_circles['S'], = self.phasor_plot.canvas.axes.plot(np.zeros(360), np.zeros(360), 'c', zorder=4, lw=1, ls='--')
+        self.phasor_circles['U'], = self.phasor_plot.canvas.axes.plot(
+            np.zeros(360), np.zeros(360), 'b', zorder=2, lw=1, ls='--')
+        self.phasor_circles['I'], = self.phasor_plot.canvas.axes.plot(
+            np.zeros(360), np.zeros(360), 'r', zorder=3, lw=1, ls='--')
+        self.phasor_circles['S'], = self.phasor_plot.canvas.axes.plot(
+            np.zeros(360), np.zeros(360), 'c', zorder=4, lw=1, ls='--')
 
         self.phasor_values = {}
-        self.phasor_values['U'] = self.phasor_plot.canvas.axes.axvline(color='b', zorder=11, lw=1, ls='--')
-        self.phasor_values['I'] = self.phasor_plot.canvas.axes.axvline(color='r', zorder=21, lw=1, ls='--')
-        self.phasor_values['P'] = self.phasor_plot.canvas.axes.axvline(color='c', zorder=31, lw=1, ls='--')
-        self.phasor_values['Q'] = self.phasor_plot.canvas.axes.axhline(color='c', zorder=32, lw=1, ls='--')
+        self.phasor_values['U'] = self.phasor_plot.canvas.axes.axvline(
+            color='b', zorder=11, lw=1, ls='--')
+        self.phasor_values['I'] = self.phasor_plot.canvas.axes.axvline(
+            color='r', zorder=21, lw=1, ls='--')
+        self.phasor_values['P'] = self.phasor_plot.canvas.axes.axvline(
+            color='c', zorder=31, lw=1, ls='--')
+        self.phasor_values['Q'] = self.phasor_plot.canvas.axes.axhline(
+            color='c', zorder=32, lw=1, ls='--')
 
     def init_sinewave_plot(self):
-        self.sinewave_plot.canvas.axes.set_ylim([-2,2])
-        self.sinewave_plot.canvas.axes.set_xlim([-np.pi,3*np.pi])
+        self.sinewave_plot.canvas.axes.set_ylim([-2, 2])
+        self.sinewave_plot.canvas.axes.set_xlim([-np.pi, 3*np.pi])
         self.sinewave_plot.canvas.axes.grid(True)
-        
-        self.sinewave_timelines = {}
-        self.sinewave_timelines[-1] = self.sinewave_plot.canvas.axes.axvline(x=-2*np.pi, color='black', zorder=1, lw=1, ls='--')
-        self.sinewave_timelines[0] = self.sinewave_plot.canvas.axes.axvline(x=0, color='black', zorder=1, lw=1, ls='--')
-        self.sinewave_timelines[1] = self.sinewave_plot.canvas.axes.axvline(x=2*np.pi, color='black', zorder=1, lw=1, ls='--')
 
-        self.phi = np.arange(-np.pi,3*np.pi,4*np.pi/1000)
+        self.sinewave_timelines = {}
+        self.sinewave_timelines[-1] = self.sinewave_plot.canvas.axes.axvline(
+            x=-2*np.pi, color='black', zorder=1, lw=1, ls='--')
+        self.sinewave_timelines[0] = self.sinewave_plot.canvas.axes.axvline(
+            x=0, color='black', zorder=1, lw=1, ls='--')
+        self.sinewave_timelines[1] = self.sinewave_plot.canvas.axes.axvline(
+            x=2*np.pi, color='black', zorder=1, lw=1, ls='--')
+
+        self.phi = np.arange(-np.pi, 3*np.pi, 4*np.pi/1000)
 
         self.sinewave_lines = {}
-        self.sinewave_lines['U'], = self.sinewave_plot.canvas.axes.plot(self.phi, np.zeros(len(self.phi)), 'b', zorder=11)
-        self.sinewave_lines['I'], = self.sinewave_plot.canvas.axes.plot(self.phi, np.zeros(len(self.phi)), 'r', zorder=21)
-        self.sinewave_lines['S'], = self.sinewave_plot.canvas.axes.plot(self.phi, np.zeros(len(self.phi)), 'c', zorder=31)
-        # self.sinewave_lines['P'], = self.sinewave_plot.canvas.axes.plot(self.phi, np.zeros(len(self.phi)), 'g', zorder=41)
-        # self.sinewave_lines['Q'], = self.sinewave_plot.canvas.axes.plot(self.phi, np.zeros(len(self.phi)), 'm', zorder=51)
+        self.sinewave_lines['U'], = self.sinewave_plot.canvas.axes.plot(
+            self.phi, np.zeros(len(self.phi)), 'b', zorder=11)
+        self.sinewave_lines['I'], = self.sinewave_plot.canvas.axes.plot(
+            self.phi, np.zeros(len(self.phi)), 'r', zorder=21)
+        self.sinewave_lines['S'], = self.sinewave_plot.canvas.axes.plot(
+            self.phi, np.zeros(len(self.phi)), 'c', zorder=31)
+        # self.sinewave_lines['P'], = self.sinewave_plot.canvas.axes.plot(
+        #    self.phi, np.zeros(len(self.phi)), 'g', zorder=41)
+        # self.sinewave_lines['Q'], = self.sinewave_plot.canvas.axes.plot(
+        #    self.phi, np.zeros(len(self.phi)), 'm', zorder=51)
 
         self.sinewave_valuelines = {}
-        self.sinewave_valuelines['U'] = self.sinewave_plot.canvas.axes.axhline(color='b', zorder=11, lw=1, ls='--')
-        self.sinewave_valuelines['I'] = self.sinewave_plot.canvas.axes.axhline(color='r', zorder=11, lw=1, ls='--')
-        self.sinewave_valuelines['S'] = self.sinewave_plot.canvas.axes.axhline(color='c', zorder=11, lw=1, ls='--')
+        self.sinewave_valuelines['U'] = self.sinewave_plot.canvas.axes.axhline(
+            color='b', zorder=11, lw=1, ls='--')
+        self.sinewave_valuelines['I'] = self.sinewave_plot.canvas.axes.axhline(
+            color='r', zorder=11, lw=1, ls='--')
+        self.sinewave_valuelines['S'] = self.sinewave_plot.canvas.axes.axhline(
+            color='c', zorder=11, lw=1, ls='--')
 
     def update_plots(self, inst_phi=0):
-        U = lambda phi=0: self.voltage_amplitude.value()/100 * np.exp(1j * self.voltage_phase_angle.value()/180*np.pi) * np.exp(1j*phi)
-        I = lambda phi=0: self.current_amplitude.value()/100 * np.exp(1j * self.current_phase_angle.value()/180*np.pi) * np.exp(1j*phi)
+        U = lambda phi=0: self.voltage_amplitude.value()/100 * np.exp(
+            1j * self.voltage_phase_angle.value()/180*np.pi) * np.exp(1j*phi)
+        I = lambda phi=0: self.current_amplitude.value()/100 * np.exp(
+            1j * self.current_phase_angle.value()/180*np.pi) * np.exp(1j*phi)
         S1 = lambda phi=0: U(phi) * I(phi)
         S0 = lambda phi=0: U(phi) * np.conj(I(phi))
         S = lambda phi=0: S0(phi) + S1(phi)
@@ -127,24 +158,36 @@ class PowerPlotApp(QMainWindow):
         inst_phi_deg = self.instantaneous_phase_angle.value() - 90
         inst_phi_rad = inst_phi_deg / 180 * np.pi
         self.instantaneous_phase_display.display(inst_phi_deg)
-    
+
         # plot phasor lines
-        self.phasor_lines['U'].set_ydata([0,np.imag(U(inst_phi_rad))])
-        self.phasor_lines['U'].set_xdata([0,np.real(U(inst_phi_rad))])
-        self.phasor_lines['I'].set_ydata([0,np.imag(I(inst_phi_rad))])
-        self.phasor_lines['I'].set_xdata([0,np.real(I(inst_phi_rad))])
-        self.phasor_lines['S'].set_ydata([np.imag(S0(inst_phi_rad)),np.imag(S(inst_phi_rad))])
-        self.phasor_lines['S'].set_xdata([np.real(S0(inst_phi_rad)),np.real(S(inst_phi_rad))])
+        self.phasor_lines['U'].set_ydata(
+            [0, np.imag(U(inst_phi_rad))])
+        self.phasor_lines['U'].set_xdata(
+            [0, np.real(U(inst_phi_rad))])
+        self.phasor_lines['I'].set_ydata(
+            [0, np.imag(I(inst_phi_rad))])
+        self.phasor_lines['I'].set_xdata(
+            [0, np.real(I(inst_phi_rad))])
+        self.phasor_lines['S'].set_ydata(
+            [np.imag(S0(inst_phi_rad)), np.imag(S(inst_phi_rad))])
+        self.phasor_lines['S'].set_xdata(
+            [np.real(S0(inst_phi_rad)), np.real(S(inst_phi_rad))])
         # self.phasor_lines['Q'].set_ydata([0,np.imag(S1())])
         # self.phasor_lines['P'].set_xdata([0,np.real(S1())])
 
         # plot phasor circles
-        self.phasor_circles['U'].set_xdata(np.real(U(np.arange(0,2*np.pi,np.pi/180))))
-        self.phasor_circles['U'].set_ydata(np.imag(U(np.arange(0,2*np.pi,np.pi/180))))
-        self.phasor_circles['I'].set_xdata(np.real(I(np.arange(0,2*np.pi,np.pi/180))))
-        self.phasor_circles['I'].set_ydata(np.imag(I(np.arange(0,2*np.pi,np.pi/180))))
-        self.phasor_circles['S'].set_xdata(np.real(S(np.arange(0,2*np.pi,np.pi/180))))
-        self.phasor_circles['S'].set_ydata(np.imag(S(np.arange(0,2*np.pi,np.pi/180))))
+        self.phasor_circles['U'].set_xdata(
+            np.real(U(np.arange(0, 2*np.pi, np.pi/180))))
+        self.phasor_circles['U'].set_ydata(
+            np.imag(U(np.arange(0, 2*np.pi, np.pi/180))))
+        self.phasor_circles['I'].set_xdata(
+            np.real(I(np.arange(0, 2*np.pi, np.pi/180))))
+        self.phasor_circles['I'].set_ydata(
+            np.imag(I(np.arange(0, 2*np.pi, np.pi/180))))
+        self.phasor_circles['S'].set_xdata(
+            np.real(S(np.arange(0, 2*np.pi, np.pi/180))))
+        self.phasor_circles['S'].set_ydata(
+            np.imag(S(np.arange(0, 2*np.pi, np.pi/180))))
 
         # plot phasor values
         self.phasor_values['U'].set_xdata(np.real(U(inst_phi_rad))*np.ones(2))
@@ -156,7 +199,6 @@ class PowerPlotApp(QMainWindow):
         self.phasor_plot.canvas.draw()
         self.phasor_plot.canvas.flush_events()
 
-
         # update sinewave lines
         self.sinewave_lines['U'].set_ydata(np.real(U(self.phi)))
         self.sinewave_lines['I'].set_ydata(np.real(I(self.phi)))
@@ -164,25 +206,30 @@ class PowerPlotApp(QMainWindow):
         # self.sinewave_lines['P'].set_ydata(np.)
 
         # update sinewave timelines
-        self.sinewave_timelines[-1].set_xdata((inst_phi_rad-2*np.pi)*np.ones(2))
-        self.sinewave_timelines[0].set_xdata((inst_phi_rad)*np.ones(2))
-        self.sinewave_timelines[1].set_xdata((inst_phi_rad+2*np.pi)*np.ones(2))
+        self.sinewave_timelines[-1].set_xdata(
+            (inst_phi_rad-2*np.pi)*np.ones(2))
+        self.sinewave_timelines[0].set_xdata(
+            (inst_phi_rad)*np.ones(2))
+        self.sinewave_timelines[1].set_xdata(
+            (inst_phi_rad+2*np.pi)*np.ones(2))
 
-        self.sinewave_valuelines['U'].set_ydata(np.real(U(inst_phi_rad))*np.ones(2))
-        self.sinewave_valuelines['I'].set_ydata(np.real(I(inst_phi_rad))*np.ones(2))
-        self.sinewave_valuelines['S'].set_ydata(np.real(S(inst_phi_rad))*np.ones(2))
+        self.sinewave_valuelines['U'].set_ydata(
+            np.real(U(inst_phi_rad))*np.ones(2))
+        self.sinewave_valuelines['I'].set_ydata(
+            np.real(I(inst_phi_rad))*np.ones(2))
+        self.sinewave_valuelines['S'].set_ydata(
+            np.real(S(inst_phi_rad))*np.ones(2))
 
         # refresh sinewave plot
         self.sinewave_plot.canvas.draw()
         self.sinewave_plot.canvas.flush_events()
 
-
     def set_instantaneous_phase(self, phase=0):
         self.instantaneous_phase_angle.setValue(phase+90)
 
     def increment_instantaneous_phase(self):
-        self.instantaneous_phase_angle.setValue(self.instantaneous_phase_angle.value()+1)
-
+        self.instantaneous_phase_angle.setValue(
+            self.instantaneous_phase_angle.value()+1)
 
 
 if __name__ == '__main__':
